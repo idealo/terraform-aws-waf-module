853064e58a2f5dab9a3e599299863b26b66f6a21
Manuel Marquez <manuel.marquez@idealo.de>
2023-10-18 12:25:36 +0200

    Fix Errors

--- data_sources.tf
+++ data_sources.tf
@@ -7,19 +7,16 @@ data "aws_caller_identity" "current" {}
 data "aws_secretsmanager_secret" "secret_header" {
   provider   = aws.fm
   arn        = var.secret_header_arn
-  depends_on = [aws_secretsmanager_secret.secret_header]
 }
 
 data "aws_secretsmanager_secret_version" "secret_header_current" {
   provider      = aws.fm
-  depends_on    = [aws_secretsmanager_secret_version.secret_header]
   secret_id     = data.aws_secretsmanager_secret.secret_header.arn
   version_stage = "AWSCURRENT"
 }
 
 data "aws_secretsmanager_secret_version" "secret_header_previous" {
   provider      = aws.fm
-  depends_on    = [aws_secretsmanager_secret_version.secret_header]
   secret_id     = data.aws_secretsmanager_secret.secret_header.arn
   version_stage = "AWSPREVIOUS"
 }
